812b3729c9095b56f235d3708415105de06170ce
yuziwoo <yuziwoo@naver.com>
2024-03-27 17:04:59 +0900

    chore: gradient 이미지 추가

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,9 @@
 import { PageStyle as S } from './MainPage.styled';
 
 const MainPage = () => {
-  return <S.Page></S.Page>;
+  return <S.Page>
+    <S.GradientBGSection></S.GradientBGSection>
+  </S.Page>;
 };
 
 export default MainPage;
